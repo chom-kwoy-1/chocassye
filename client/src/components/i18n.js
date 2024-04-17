27b0459f646cbfd1bec600961b31c8c9df42521c
Chom.kwoy <chom.kwoy@xn--gt1b.xyz>
2024-04-18 05:04:10 +0900

    comment out korean lang

--- client/src/components/i18n.js
+++ client/src/components/i18n.js
@@ -96,7 +96,7 @@ i18n
   .use(LanguageDetector)
   .init({
     resources,
-    lng: "ko", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    //lng: "ko", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
