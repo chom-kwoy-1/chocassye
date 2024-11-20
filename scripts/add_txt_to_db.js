fcaa2f2ed1821efb39e0d165461ecbe53a730f29
Chomkwoy <chom.kwoy@xn--gt1b.xyz>
2024-11-20 19:27:08 +0900

    add format 2

--- scripts/add_txt_to_db.js
+++ scripts/add_txt_to_db.js
@@ -6,7 +6,8 @@ import glob from "glob";
 import fs from "fs";
 import {insert_into_db} from "./insert_into_db.js";
 import {parse_year_string, year_and_bookname_from_filename} from "./parse_utils.js";
-import {hangul_to_yale} from "../client/src/components/YaleToHangul.mjs";
+import {hangul_to_yale, normalize_string} from "../client/src/components/YaleToHangul.mjs";
+import path from "path";
 
 function parse_format_1(file, lines) {
     const hasImages = false;
@@ -92,6 +93,112 @@ function parse_format_1(file, lines) {
     return [ book_details, sentences ];
 }
 
+function parse_format_2(file, lines) {
+    const hasImages = false;
+
+    file = file.normalize('NFKC')
+    let filename = path.parse(file).name;
+
+    let year_string = "미상";
+    let splits = filename.split('_');
+    if (splits.length > 1) {
+        filename = splits.splice(1).join(' ');
+        year_string = splits[0];
+    }
+
+    let {year, year_start, year_end} = parse_year_string(year_string);
+
+    const attributions = [];  // TODO
+    const bibliography = file.includes("장서각")? "장서각" : null;
+
+    let book_details = {
+        filename: filename,
+        year: year,
+        year_sort: year ?? 9999,
+        decade_sort: year === null ? 9999 : Math.floor(year / 10) * 10,
+        year_start: year_start,
+        year_end: year_end,
+        year_string: year_string,
+        attributions: attributions,
+        bibliography: bibliography,
+    };
+
+    let sentences = [];
+
+    let pageno = "";
+    let index = 0;
+    let number_in_page = 0;
+
+    const xml_tag_re = /<[^>]*?>/g;
+    const page_tag_re = /<page id="([^"]*)"\/>/;
+
+    for (let i = 0; i < lines.length; i++) {
+        let line = lines[i].trim();
+        if (line === "") {
+            continue;
+        }
+
+        const page_tag = line.match(page_tag_re);
+        if (page_tag !== null) {
+            pageno = page_tag[1];
+            number_in_page = 0;
+        }
+
+        line = line.replaceAll('+', '^');  // page break marker
+        line = line.replaceAll('ː', ' ');
+        line = line.replaceAll('[격자1]', '');
+        line = line.replaceAll('[격자2]', '');
+        line = line.replaceAll('[대두1]', '');
+        line = normalize_string(line);
+        line = line.replaceAll('<title>', '[title]');
+        line = line.replaceAll('</title>', '.');
+        line = line.replace(xml_tag_re, '').trim();  // remove xml tags
+        line = line.replace('/', ' @ ');  // stanza break marker
+        line = line.replace(/\s+/g, ' ');  // remove extra spaces
+
+        for (let sent of line.split('.')) {
+            sent = sent.trim();
+            if (sent === "") {
+                continue;
+            }
+
+            let type = "main";
+            if (sent.includes("[title]")) {
+                type = "title";
+                sent = sent.replaceAll('[title]', '');
+            }
+
+            const text = hangul_to_yale(sent);
+            const text_without_sep = text.replace(/[ .^]/g, '');
+
+            sentences.push({
+                filename: filename,
+                text: text,
+                text_without_sep: text_without_sep,
+                text_with_tone: null,
+                html: text,
+                type: type,
+                lang: null,
+                page: pageno,
+                orig_tag: "sent",
+                number_in_page: number_in_page,
+                number_in_book: index,
+                hasImages: hasImages,
+                year_sort: book_details.year_sort,
+                decade_sort: book_details.decade_sort,
+            });
+
+            number_in_page = number_in_page + 1;
+            index = index + 1;
+        }
+    }
+
+    book_details['num_sentences'] = sentences.length;
+    book_details['non_chinese_sentence_count'] = sentences.length;
+
+    return [ book_details, sentences ];
+}
+
 function add_text_file(file, text) {
     const lines = text.split("\n");
 
@@ -100,7 +207,14 @@ function add_text_file(file, text) {
     }
 
     if (lines[0].includes('<title>')) {
+        const SKIP_FORMAT_1 = (process.env.SKIP_FORMAT_1 || "no") === "yes";
+        if (SKIP_FORMAT_1) {
+            throw new Error("Skipping format 1: " + file);
+        }
         return parse_format_1(file, lines);
+    }
+    else if (lines[0].startsWith('<item ')) {
+        return parse_format_2(file, lines);
     }
     else {
         throw new Error("Unknown format: " + file);
@@ -113,16 +227,21 @@ function insert_documents(pool) {
     ).then(async files => {
         console.log("Total", files.length, "files");
 
+        let inserted_books = [];
         let promises = [];
         for (let [i, file] of files.entries()) {
             const pushTask = promisify(fs.readFile)(file, "utf8")
                 .then(async text => {
                     text = text.replace(/^\uFEFF/, '');  // remove BOM
                     const [book_details, sentences] = add_text_file(file, text);
-                    return insert_into_db(pool, book_details, sentences);
+                    const DRY_RUN = (process.env.DRY_RUN || "no") === "yes";
+                    if (!DRY_RUN) {
+                        return insert_into_db(pool, book_details, sentences);
+                    }
                 })
                 .then(() => {
                     console.log(i, "DONE", file);
+                    inserted_books.push(file);
                 })
                 .catch((err) => {
                     console.error(i, "ERROR", file, err.code, err.stack);
@@ -137,6 +256,11 @@ function insert_documents(pool) {
             }
         }
         await Promise.all(promises);
+
+        console.log("Inserted", inserted_books.length, "books:");
+        for (let book of inserted_books) {
+            console.log(book);
+        }
     });
 }
 
